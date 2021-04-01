61a76604d1cdecdea0d92234ba83c11866aee7a0
Ramazan Polat <ramazanpolat@gmail.com>
2021-04-01 23:34:23 +0300

    paralel inserts

--- loader.sh
+++ loader.sh
@@ -6,7 +6,7 @@ while true; do
       if [[ "$file" == *user* ]]; then
         printf 'loading %s ...' "$file"
         # asagidaki satiri kendinize gore degistiriniz
-        docker run -i --rm --link clickhouse:clickhouse yandex/clickhouse-client:20.8.12.2 --host clickhouse -q "insert into user_detail(user_detail_id,user_id,mbb,device_client_id,device_client_ip,device_os,is_cracked,connection_type,imei,carrier,operation,insert_date,insert_time,is_success,msisdn,type) FORMAT JSONEachRow SETTINGS max_insert_threads=16" < $file
+        docker run -i --rm --link clickhouse:clickhouse yandex/clickhouse-client --host clickhouse -q "insert into user_detail(user_detail_id,user_id,mbb,device_client_id,device_client_ip,device_os,is_cracked,connection_type,imei,carrier,operation,insert_date,insert_time,is_success,msisdn,type) FORMAT JSONEachRow SETTINGS max_insert_threads=16" < $file
         printf 'done.\n'
         sleep 0.1
       fi
@@ -14,7 +14,7 @@ while true; do
       if [[ "$file" == *fraud* ]]; then
         printf 'loading %s ...' "$file"
         # asagidaki satiri kendinize gore degistiriniz
-        docker run -i --rm --link clickhouse:clickhouse yandex/clickhouse-client:20.8.12.2 --host clickhouse -q "insert into fraud_result(record_date,mbb,device_uid,query_name,source,package_list,device_client_id,device_info,type) FORMAT JSONEachRow SETTINGS max_insert_threads=16" < $file
+        docker run -i --rm --link clickhouse:clickhouse yandex/clickhouse-client --host clickhouse -q "insert into fraud_result(record_date,mbb,device_uid,query_name,source,package_list,device_client_id,device_info,type) FORMAT JSONEachRow SETTINGS max_insert_threads=16" < $file
         printf 'done.\n'
         sleep 0.1
       fi
